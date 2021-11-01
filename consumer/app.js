c3f4664628c7998626e28d371722de17b955ff51
czoido <mrgalleta@gmail.com>
2021-11-01 09:16:01 +0100

    minor changes

--- consumer/app.js
+++ consumer/app.js
@@ -3,10 +3,10 @@ const cpp_funtions = require('./bin/cpp-functions.js')
 
 c_funtions().then((instance) => {
     var ZlibVersion = instance.cwrap('ZlibVersion', 'string', []);
-    console.log(ZlibVersion());
+    console.log("zlib version is: " + ZlibVersion());
 });
 
 cpp_funtions.onRuntimeInitialized = async _ => {
     zlibVersion = cpp_funtions.ZlibVersion();
-    console.log(zlibVersion);
+    console.log("zlib version is: " + zlibVersion);
 }
